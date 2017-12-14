45ab55fc750b359bcdc47ebd42d976b1717bc011
Georgie Santiago <georgies@rminno.com>
2017-12-14 13:46:34 -0500

    but fix Unable to find Application\App class

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,5 @@
 */
 
 Route::get('/', function () {
-//  $json = file_get_contents("https://www.cryptocompare.com/api/data/price/");
-  $json = file_get_contents("https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR");
-  $data = json_decode($json);
-  dd($data);
-  $coins = $data->Data;
-  echo "<select>";
-  foreach($coins as $coin)
-  {
-    echo "<option value='" . $coin->Id . "' >" . $coin->CoinName . "</option>";
-  }
-  echo "</select>";
-  exit;
+    return view('welcome');
 });
