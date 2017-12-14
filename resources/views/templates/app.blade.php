1e93590aa8ae5851d2e10f7bbfee936742a8167c
Georgie Santiago <georgies@rminno.com>
2017-12-14 14:12:46 -0500

    Added Authentication Scafolding

---
+++ resources/views/templates/app.blade.php
@@ -0,0 +1,16 @@
+@section('header')
+  <html>
+    <head>
+      <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    </head>
+
+    <body>
+      <div class="container">
+          @yield('content')
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+         <!-- Include all compiled plugins (below), or include individual files as needed -->
+         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+      </body>
+      </html>
