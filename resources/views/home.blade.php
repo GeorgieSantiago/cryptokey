43dac7cbba3157a81bad096e02355465b1e36faf
GeorgieSantiago <milestoneartisan@gmail.com>
2017-12-15 11:56:29 -0500

    Add new assets for view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -13,11 +13,58 @@
                             {{ session('status') }}
                         </div>
                     @endif
+<!---->
+<?php
 
-                    You are logged in!
+$data = $coins['CoinList']->Data;
+$price = $coins['PriceExample'];
+?>
+<div class="table-heading">
+  <h2>All Coins</h2>
+</div>
+<div class="agile-tables">
+  <div class="w3l-table-info">
+    <h3></h3>
+      <table id="table" class="table">
+    <thead>
+      <tr>
+      <th>Coin Symbol</th>
+      <th>Id</th>
+      <th>Price</th>
+      <th>data</th>
+      <th>data</th>
+      <th>data</th>
+      </tr>
+    </thead>
+    <tbody>
+@foreach($data as $symbol => $info)
+      <tr>
+          <td>{{$symbol}}</td>
+          <td>{{$info->Id}}</td>
+          @foreach($price as $name => $value)
+            @if($name == $symbol || $name . "E" == $symbol || "BIT" . $name == $symbol)
+              <td>{{$value}}</td>
+            @else
+              <?php continue; ?>
+            @endif
+          @endforeach
+      </tr>
+@endforeach
+</tbody>
+</table>
+</div>
+<script class="js">
+$('#table').basictable();
+</script>
+
+<!---->
+
+
+
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,6 @@
-<p>Landing Page</p>
+@extends('layouts.app')
+
+@section('content')
+<h1>Welcome</h1>
+<a class="btn btn-lg" href="{{route('home')}}">Go to the dashboard</a>
+@endsection
